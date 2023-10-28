ba7b08cddc2898bc77c4f134543a9f0a76db3814
Ahemd <ahmedsaye23156@gmail.com>
2023-10-28 11:26:10 +0200

    new other way

--- test.py
+++ test.py
@@ -1,10 +1,3 @@
-tracker = 0
-for i in range(1,15):
-    # tracker += 0
-    if i == 10:
-        break
-    tracker += 1  # you did {tracker times}  
-    print(i)
+s = "abcdefgh" 
 
-print(tracker)
-    
+print(s[1:6:2])
