c44002f23bb97f6bf58d0e4043e7eabfb29dfb36
Ahemd <ahmedsaye23156@gmail.com>
2023-10-28 10:55:27 +0200

    how to choose a good answer

--- test.py
+++ test.py
@@ -1,19 +1,5 @@
-# let's create a binary search algorithms
+import random as A
+names = ["Ahmed","Nada","sara","Omar"]
+B =A.randint(0,len(names)-1)
 
-
-def search(arr,item):
-    high= len(arr)- 1
-    low = 0
-    while low <= high:
-        mid = high + low
-        guess = arr[mid]
-        if guess == item:
-            return mid
-        elif guess > item:
-            high = mid -1
-        else:
-            low = mid +1
-    return None
-
-
-print(search([1,2,4,55,77], 77))
+print(names[B])
