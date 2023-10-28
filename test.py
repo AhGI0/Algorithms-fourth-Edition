35a737336db7c06e6739eed4aff6928a22afe006
Ahemd <ahmedsaye23156@gmail.com>
2023-10-28 11:34:17 +0200

    so amzing

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
-A ="Nhmed"
-
-B = "A"+ A[1::]
-print(B)
+def check(str):
+    if str == "Nada":
+        return("it's okay")
+    else:
+        the_diff =str[0]
+        return(f'the word will be N{str[1::]} the wrong letter is {the_diff}')
+print(check("Hada"))
