33178514c9b97cda53a7020dcfdc0441a800fde4
Ahemd <ahmedsaye23156@gmail.com>
2023-10-28 09:08:32 +0200

    new other ways

--- test.py
+++ test.py
@@ -1,17 +1,19 @@
-# you just forget about it 
+# let's create a binary search algorithms
+
+
 def search(arr,item):
-    low= 0
-    high = len(arr)-1
+    high= len(arr)- 1
+    low = 0
     while low <= high:
-        mid = (low+ high)
-        guess= arr[mid]
-        if item == guess:
+        mid = high + low
+        guess = arr[mid]
+        if guess == item:
             return mid
-        elif item < guess:
+        elif guess > item:
             high = mid -1
         else:
-            low = mid + 1
+            low = mid +1
     return None
 
 
-print(search([1,3,5,7,8,9],8))
+print(search([1,2,4,55,77], 77))
