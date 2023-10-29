8611689ff9cb71319afdf525d8bbb265089c4594
Ahemd <ahmedsaye23156@gmail.com>
2023-10-29 12:46:40 +0200

    new

--- test.py
+++ test.py
@@ -1,18 +1,1 @@
-# binary search 
-
-def search(arr,item):
-    low =0
-    high = len(arr)-1
-    while low <= high:
-        mid =low + high
-        guess = arr[mid]
-        if guess == item:
-            return mid
-        elif guess <item:
-            low = mid +1
-        else:
-            high = mid + 1
-    return None
-
-
-print(search([1,1,5,7,8,9,10],10))
+print(abs(-5))
