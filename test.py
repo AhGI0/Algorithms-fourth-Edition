4a376f3f868fef3cd920aa33e559bc71d40876e9
Ahemd <ahmedsaye23156@gmail.com>
2023-10-29 20:24:53 +0200

    .

--- test.py
+++ test.py
@@ -1,5 +1,20 @@
+# how to find the cube root
 
-A=["ahmed","sayed"]
+
+low= 0
+cube = 125 
+high = cube
+value = (high + low) / 2
+epsion = .01
+num_guesses = 0
+
+while abs(value ** 3 - cube) >= epsion:
+    if value **3 < cube:
+        low = value
+    else:
+        high = value
+    value =(high +low)/2
+    num_guesses += 1
 
 
-print(A[0])
+print(int(value))  #
