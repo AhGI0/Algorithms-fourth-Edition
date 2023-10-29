014aac49c1f5321bb08366d803cd7e60e5205545
Ahemd <ahmedsaye23156@gmail.com>
2023-10-29 15:00:44 +0200

    new areas always hits you hard

--- test.py
+++ test.py
@@ -1,17 +1,5 @@
-cube = 27
-high = cube 
-low = 0
-guess =(high + low) / 2
-num_guesses = 0
-epsilon = .01
 
-while abs((guess **3 - cube)) >= epsilon:
-    if guess **3 <cube:
-        low = guess
-    else:
-        high = guess
-    guess =(low+ high) / 2
-    num_guesses += 1
+A=["ahmed","sayed"]
 
 
-print(f'the cube root of {cube} is {guess}')
+print(A[0])
