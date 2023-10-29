765c0b70553b12464b502727ed0f7bc381c5eb5f
Ahemd <ahmedsaye23156@gmail.com>
2023-10-29 14:00:21 +0200

    new

--- test.py
+++ test.py
@@ -1,19 +1,17 @@
-# let's play bisection algorithm again 
-
+cube = 27
+high = cube 
 low = 0
-epison = .01
-cube = int(input("Enter a Number: "))
-high =cube
-guess = (low + high) / 2
-num_guesses =0
+guess =(high + low) / 2
+num_guesses = 0
+epsilon = .01
 
-# you can play any root [square root , cube root]
-while abs((guess ** 2 - cube)) >= epison:
-    if guess **2 < cube:
+while abs((guess **3 - cube)) >= epsilon:
+    if guess **3 <cube:
         low = guess
     else:
-        high =guess
-    guess =((low + high) / 2)
+        high = guess
+    guess =(low+ high) / 2
     num_guesses += 1
+
 
 print(f'the cube root of {cube} is {guess}')
