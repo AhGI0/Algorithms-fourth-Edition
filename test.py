5ffd6dc17dadfed1b7f4a85f449f7c358c573967
Ahemd <ahmedsaye23156@gmail.com>
2023-10-29 20:33:44 +0200

    new

--- test.py
+++ test.py
@@ -1,20 +1,2 @@
-# how to find the cube root
+# lecture 4
 
-
-low= 0
-cube = 18 
-high = cube
-value = (high + low) / 2
-epsion = .01
-num_guesses = 0
-
-while abs(value ** 2 - cube) >= epsion:
-    if value **2 < cube:
-        low = value
-    else:
-        high = value
-    value =(high +low)/2
-    num_guesses += 1
-
-
-print((value))  #
