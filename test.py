5f68ca4e343795bd83db2e21fee4f0231b9cd868
Ahemd <ahmedsaye23156@gmail.com>
2023-10-30 11:39:59 +0200

    new array

--- test.py
+++ test.py
@@ -1,7 +1,15 @@
-A= {
-    "name":"Ahmed",
-    "age":"22",
-    "movies":"Alibaba" 
-}
+A= open("text.txt","rt")
+new= 0
+arr= []
+for line in A:
+    line = line.strip()
+    try:
+        value = int(line)
+        new +=value
+    except ValueError:
+        arr.append(line)
 
-print(A["movies"])
+
+print(new)
+
+
