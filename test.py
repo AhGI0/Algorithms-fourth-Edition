aee765c51edb86b4773044878786020dacb53456
Ahemd <ahmedsaye23156@gmail.com>
2023-10-31 10:58:32 +0200

    it will call each code

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
-def check(ex):
-    A =ex
-    return A /2
+def g(x):
+    def h():
+        x ="abc"  # it's conestant
+        print(x) 
+    x = x+ 1
+    print(f'g:x = {x}')
+    h()
+    return x
+x= 3
+z= g(x)
 
-print(check(10))
