512e520de4466c1f8487f02d6b0a29347dbe75ff
Ahemd <ahmedsaye23156@gmail.com>
2023-10-31 12:12:29 +0200

    amazing

--- test.py
+++ test.py
@@ -1,6 +1,19 @@
-def i(ex):
-    if ex == 0:
-        return 1
-    return ex * i(ex- 1)  #the aim of it find the factorial
+# def shelf(one,two):
+#     if two ==0:
+#         return 0 
+#     else:
+#         return one +shelf(one ,two-1)
+# print(shelf(10,5))
 
-print(i(4))
+# # --- 
+
+
+def shelf(one,two):
+    result =0 
+    while two > 0:
+        result += one
+        two -= 1
+    return result
+
+
+print(shelf(10,3))   #expected to be 300
