929c861e4bd92b42bd7414ea7b45c42b335c49c7
Ahemd <ahmedsaye23156@gmail.com>
2023-10-31 11:48:11 +0200

    new

--- test.py
+++ test.py
@@ -1,9 +1,6 @@
-# how to make a specifications in loops 
+A= [1,2]
+B= A[:]
 
-def copy(one,two):
-    A= one[:]
-    for i in A:
-        if i in two:
-            one.remove(i)
-    return one
-print(copy([1,2,3,4,5],[4,5]))
+B.extend([4,5])
+
+print(A)
