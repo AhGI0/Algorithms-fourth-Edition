5ea70f58e4e46fbe2dd1dee19a9487930b4bf8cc
Ahemd <ahmedsaye23156@gmail.com>
2023-10-31 12:23:15 +0200

    new other lands

--- test.py
+++ test.py
@@ -1,19 +1,7 @@
-# def shelf(one,two):
-#     if two ==0:
-#         return 0 
-#     else:
-#         return one +shelf(one ,two-1)
-# print(shelf(10,5))
-
-# # --- 
+def fact(Ex):
+    if Ex == 0:
+        return 1
+    return Ex*(fact(Ex-1))
 
 
-def shelf(one,two):
-    result =0 
-    while two > 0:
-        result += one
-        two -= 1
-    return result
-
-
-print(shelf(10,3))   #expected to be 300
+print(fact(4))  # 4*3*2*1
