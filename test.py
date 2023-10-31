073955b385f3ff0d978936171e5474b6b0d1d602
Ahemd <ahmedsaye23156@gmail.com>
2023-10-31 11:22:47 +0200

    new

--- test.py
+++ test.py
@@ -1,9 +1,14 @@
-def sums(one,two):
-    add = one +two
-    times =one * two
-    return(add,times)
+def get_data(ex):
+    num = ()
+    word = ()
+    for i in ex:
+        num += (i[0],)
+        if i[1] not in word:
+            word += (i[1],)
+    min_n = min(num)
+    max_n = max(num)
+    unique = len(word)
+    return(min_n,max_n,unique)
 
-(A,B)= sums(10,15) 
 
-print(A)
-print(B)
+print(get_data(((10,"Ahmed"),(100,"Nada"),(400,"Yosra"))))
